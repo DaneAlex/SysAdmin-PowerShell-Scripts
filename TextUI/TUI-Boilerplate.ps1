214a03ff04344005b96eaffa9b7b9443c00cea2f
Dane Patrick <32422882+DaneAlex@users.noreply.github.com>
2021-06-07 11:01:47 -0500

    Create TUI-Boilerplate.ps1

---
+++ TextUI/TUI-Boilerplate.ps1
@@ -0,0 +1,23 @@
+Import-Module Microsoft.PowerShell.ConsoleGuiTools 
+$module = (Get-Module Microsoft.PowerShell.ConsoleGuiTools -List).ModuleBase
+Add-Type -Path (Join-path $module Terminal.Gui.dll)
+
+# INITIALIZE TUI
+[Terminal.Gui.Application]::Init()
+$Window = [Terminal.Gui.Window]::new()
+$Window.Title = "PoSH - TUI Example"
+[Terminal.Gui.Application]::Top.Add($Window)
+
+# FILE MENU
+$FileMenuItem = [Terminal.Gui.MenuItem]::new("Exit", "", { [Terminal.Gui.Application]::Shutdown() })
+$FileMenuBarItem = [Terminal.Gui.MenuBarItem]::new("File", @($FileMenuItem))
+
+# HELP MENU
+$AboutMenuItem = [Terminal.Gui.MenuItem]::new("About", "", { [Terminal.Gui.MessageBox]::Query("About", "Simple TUI Example") })
+$HelpMenuBarItem = [Terminal.Gui.MenuBarItem]::new("Help", @($AboutMenuItem))
+
+$MenuBar = [Terminal.Gui.MenuBar]::new(@($FileMenuBarItem, $HelpMenuBarItem))
+
+$Window.Add($MenuBar)
+
+[Terminal.Gui.Application]::Run()
